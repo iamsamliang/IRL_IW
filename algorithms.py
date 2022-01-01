28a646d28b45ad2bbedecf5fa21a83c1528e0b9e
Sam Liang <darkdragn20000@gmail.com>
2022-01-01 15:30:08 -0500

    refactored code and examples using it

--- algorithms.py
+++ algorithms.py
@@ -1,62 +1,118 @@
-from abc import ABC, abstractclassmethod
+from abc import ABC, abstractmethod
 from msdm.algorithms.entregpolicyiteration import entropy_regularized_policy_iteration
 from torch.utils.data.dataloader import DataLoader
 from dataset import TrajectoryDataset, FeaturesDataset
 import torch
 import numpy as np
-import frozendict
+from frozendict import frozendict
 
 
 class Algorithm(ABC):
-    @abstractclassmethod
-    def fit(self):
-        pass
+    @classmethod
+    @abstractmethod
+    def learn(self, dataset: TrajectoryDataset):
+        '''Takes in a dataset containing formatted trajectories, performs optimization on parameters determined by the algorithm, and returns an object containing the learned parameters
+        '''
 
 
 class MaxLikelihoodIRL(Algorithm):
-    @classmethod
-    def fit(self, mdp, trajectories, batch_size=64, epochs=50, lr=1):
+    def __init__(self, mdp, featurizer, fixed_reward, batch_size=64, epochs=50, lr=1, entropy_weight=1, planning_iters=10):
         '''
         Parameters:
-          mdp: a representation of the Markov Decision Process (x-coords, y-coords, color of states, and transition probabilities)
-          feature_matrix: S | 1 x A | 1 x S x M where the arr[i, j, k, :] = the features of taking action j in state i and transitioning to state k
-          step_cost_matrix: S x A | 1 x S | 1 - action at every state has a step cost, except the terminal state
-          trajectories: array of trajectories which are dictionaries containing the state and action trajectory
+          mdp: a representation of the Markov Decision Process
+          featurizer: func(state, action, next_state) -> dictionary w/ features as keys and float|int associated with each feature as values; unspecified features will have a value of 0
+          fixed_reward: func(state, action, next_state) -> float
           batch_size: size of a batch of data for training
           epochs: number of epochs for learning the reward function
           lr: learning rates for each iteration (scalar or array of length epochs)
+          entropy_weight: for entropy policy (see msdm entropy_regularized_policy_iteration)
+          planning_iters: for entropy policy (see msdm entropy_regularized_policy_iteration)
+        Internal Outputs:
+          feature_matrix: S | 1 x A | 1 x S x F where the arr[i, j, k, :] = the features of taking action j in state i and transitioning to state k, F = number of features
+          fixed_reward_matrix: S x A | 1 x S | 1 - action at every state has a fixed reward
+        '''
+        self.mdp = mdp
+        self.featurizer = featurizer
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.lr = lr
+        self.entropy_weight = entropy_weight
+        self.planning_iters = planning_iters
+
+        num_states = len(self.mdp.state_list)
+        num_actions = len(self.mdp.action_list)
+
+        # taking an action in a state and transitioning to another state has a fixed reward
+        fixed_reward_matrix = np.zeros((num_states, num_actions, num_states))
+        states = mdp.state_index
+        actions = mdp.action_index
+        # generate the appropriate fixed reward from user-defined fixed reward function
+        for state, state_index in states.items():
+            for action, action_index in actions.items():
+                for next_state in mdp.next_state_dist(state, action).support:
+                    next_state_index = states.get(next_state)
+                    fixed_reward_matrix[state_index, action_index, next_state_index] = fixed_reward(
+                        state, action, next_state)
+
+        # put all features that have a value other than 0 associated with it into a list for a consistent indexing purpose
+        features = set()
+        for state in states.keys():
+            for action in actions.keys():
+                for next_state in mdp.next_state_dist(state, action).support:
+                    features.update(featurizer(
+                        state, action, next_state).keys())
+        # OR all features must be listed in the dictionary no matter whether they are 0
+        features = sorted(features)
+        num_features = len(features)
+        # update feature_matrix using user-defined featurizer
+        feature_matrix = np.zeros(
+            (num_states, num_actions, num_states, num_features))
+        for state, state_index in states.items():
+            for action, action_index in actions.items():
+                for next_state in mdp.next_state_dist(state, action).support:
+                    next_state_index = states.get(next_state)
+                    for feat, feat_val in featurizer(state, action, next_state).items():
+                        assert isinstance(
+                            feat_val, (int, float)), "value associated with feature needs to be a number"
+                        feat_index = features.index(feat)
+                        feature_matrix[state_index, action_index,
+                                       next_state_index, feat_index] = feat_val
+
+        self.feature_matrix = torch.tensor(feature_matrix)
+        self.fixed_reward_matrix = torch.tensor(fixed_reward_matrix)
+
+    def learn(self, trajectories: TrajectoryDataset):
+        '''
+        Parameters:
+          trajectories: TrajectoryDataset trajectories
         Output:
           r_weights: the learned reward weights
           policy: policy associated with the final r_weights
         '''
 
-        trajs_dataset = TrajectoryDataset(trajectories)
+        trajectory_weight = 1/len(trajectories)
         trajs_dataloader = DataLoader(
-            trajs_dataset, batch_size=batch_size, shuffle=True)
-
-        feature_matrix = self.__getfeaturematrix__(mdp)
-        step_cost_matrix = self.__getstepcostmatrix__(mdp)
+            trajectories, batch_size=self.batch_size, shuffle=True, collate_fn=self.__collate_fn)
 
         # randomize weights
-        r_weights = torch.tensor(np.random.randn(feature_matrix.shape[-1]))
+        r_weights = torch.tensor(
+            np.random.randn(self.feature_matrix.shape[-1]))
+
+        print(f"Inital reward weights: {r_weights}\n")
 
         # try using the true reward weights to debug - Checked. Has basically the same rewards
         # r_weights = torch.tensor([float(mdp_params['feature_rewards'][f]) for f in features]).double()
 
         r_weights.requires_grad = True
-        discount_rate = torch.tensor(mdp.discount_rate)
-        transition_matrix = torch.tensor(mdp.transition_matrix)
+        discount_rate = torch.tensor(self.mdp.discount_rate)
+        transition_matrix = torch.tensor(self.mdp.transition_matrix)
 
-        # parameters for policy algorithm
-        entropy_weight = 1
-        planning_iters = 10
-
-        optimizer = torch.optim.SGD([r_weights], lr)
+        optimizer = torch.optim.SGD([r_weights], self.lr)
 
         size = len(trajs_dataloader.dataset)
 
-        for _ in range(epochs):
-            for batch, (state_trajectories, action_trajectories, trajectories_weights) in enumerate(trajs_dataloader):
+        for _ in range(self.epochs):
+            for batch, sample in enumerate(trajs_dataloader):
                 optimizer.zero_grad()
 
                 # build the reward matrix of the entire MDP using current reward weights
@@ -64,131 +120,143 @@ class MaxLikelihoodIRL(Algorithm):
                 # We do not have the reward weights from MDP bc the reward weights are what we are optimizing. Thus, use r_weights
                 feature_reward_matrix = torch.einsum(
                     "sanf,f->san",
-                    feature_matrix,
+                    self.feature_matrix,
                     r_weights
                 )
 
                 # construct the final reward function by taking into account each action's step cost
-                reward_matrix = feature_reward_matrix + step_cost_matrix
-
-                # this should be |S| x 1 x |S| since its the same for all actions
-                assert tuple(reward_matrix.shape) == (
-                    len(mdp.state_list), 1, len(mdp.state_list))
+                reward_matrix = feature_reward_matrix + self.fixed_reward_matrix
 
                 # anything leading to the terminal state ({-1, -1}) has zero reward
-                terminal_index = mdp.state_index.get(
+                terminal_index = self.mdp.state_index.get(
                     frozendict({'x': -1, 'y': -1}))
                 reward_matrix[:, :, terminal_index] = 0
 
                 # compute the optimal policy for the current reward function
                 policy = entropy_regularized_policy_iteration(
-                    transition_matrix, reward_matrix, discount_rate, entropy_weight, planning_iters).policy
+                    transition_matrix, reward_matrix, discount_rate, self.entropy_weight, self.planning_iters).policy
                 loss = torch.tensor(0.0)
 
-                for traj_index in range(len(state_trajectories)):
-                    state_traj = state_trajectories[traj_index]
-                    action_traj = action_trajectories[traj_index]
-                    trajectory_weight = trajectories_weights[traj_index]
+                for state_traj, action_traj in sample:
                     for index in range(len(state_traj)):
                         state = state_traj[index]
                         action = action_traj[index]
-                        state_index = mdp.state_index.get(state)
-                        action_index = mdp.action_index.get(action)
+                        state_index = self.mdp.state_index.get(state)
+                        action_index = self.mdp.action_index.get(action)
                         loss -= torch.log(policy[state_index,
-                                          action_index]) * trajectory_weight
+                                                 action_index]) * trajectory_weight
                 loss.backward()
                 optimizer.step()
 
-                if batch % 40000 == 0:
-                    loss, current = loss.item(), batch * len(state_trajectories)
+                if batch % 2 == 0:
+                    loss, current = loss.item(), batch * len(sample)
                     print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+        print(f"Final reward weights: {r_weights}\n")
 
         # compute policy from learned weights
         feature_reward_matrix = torch.einsum(
             "sanf,f->san",
-            feature_matrix,
+            self.feature_matrix,
             r_weights
         )
 
-        reward_matrix = feature_reward_matrix + step_cost_matrix
-        terminal_index = mdp.state_index.get(
+        reward_matrix = feature_reward_matrix + self.fixed_reward_matrix
+        terminal_index = self.mdp.state_index.get(
             frozendict({'x': -1, 'y': -1}))
         reward_matrix[:, :, terminal_index] = 0
         policy = entropy_regularized_policy_iteration(
-            transition_matrix, reward_matrix, discount_rate, entropy_weight, planning_iters).policy
+            transition_matrix, reward_matrix, discount_rate, self.entropy_weight, self.planning_iters)
 
         return r_weights, policy
 
-    def __getfeaturematrix__(self, mdp):
-        features = sorted(mdp.feature_locations.keys())
-        features = [f for f in features if f not in 'gs']
+    def get_feature_matrix(self):
+        return self.feature_matrix
 
-        # each state is a one-hot vector of its color
-        state_feature_matrix = torch.zeros(len(mdp.state_list), len(features))
-        states = mdp.state_index
+    def get_fixed_reward_matrix(self):
+        return self.fixed_reward_matrix
 
-        for state, state_index in states.items():
-            f = mdp.location_features.get(state, '.')
-            if f in features:
-                feature_index = features.index(f)
-            else:
-                continue
-            state_feature_matrix[state_index, feature_index] = 1
-
-        # shape is 1 (state) x 1 (action) x num_states x num_features bc the features only depend on the next_state so for any state and any action, if it ends in next_state i, it should have the features corresponding to next_state i
-        state_action_nextstate_feature_matrix = state_feature_matrix[None, None, :]
-        return state_action_nextstate_feature_matrix.double()
-
-    def __getstepcostmatrix__(self, mdp):
-        # action at every state has a step cost, except the terminal state
-        state_step_cost_matrix = torch.tensor(
-            mdp.step_cost) * mdp.nonterminal_state_vec
-
-        # shape is num_states (state) x 1 (action) x 1 (next_state) bc the step cost is only dependent on the action you take in the current state. Since it is the same for all actions (and what next_state you end up at), we only need to encode it for the 26 states
-        step_cost_matrix = state_step_cost_matrix[:, None, None]
-        return step_cost_matrix
+    def __collate_fn(self, batch):
+        return batch
 
 
 class ImitationLearning(Algorithm):
-    @classmethod
-    def fit(self, mdp, trajectories, model, loss_fn, optimizer, scheduler=None, batch_size=128, epochs=50):
+    def __init__(self, mdp, state_featurizer, action_featurizer, model, loss_fn, optimizer, scheduler=None, batch_size=128, epochs=50):
         '''
         Parameters:
           mdp: a representation of the Markov Decision Process
-          trajectories: array of trajectories which are dictionaries containing the state and action trajectory
+          state_featurizer: func(state) -> dictionary w/ features as keys and float|int associated with each feature as values; unspecified features will have a value of 0
+          action_featurizer: func(action) -> integer
           model: PyTorch neural network
           loss_fn: PyTorch loss function
           optimizer: PyTorch optimizer
           scheduler: PyTorch scheduler (varies learning rate at specific epochs)
           batch_size: size of a batch of data for training
           epochs: number of epochs for learning the reward function
+        Internal Output:
+          state_feature_matrix: S x F where the arr[i, :] = the features of state i, F = number of features
+        '''
+
+        self.mdp = mdp
+        self.model = model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.batch_size = batch_size
+        self.epochs = epochs
+
+        # num_states = len(self.mdp.state_list)
+        # states = mdp.state_index
+
+        # # put all features that have a value other than 0 associated with it into a list for a consistent indexing purpose
+        # features = set()
+        # for state in states.keys():
+        #     features.update(state_featurizer(state).keys())
+
+        # features = sorted(features)
+        # num_features = len(features)
+        # # update feature_matrix using user-defined state_featurizer
+        # state_feature_matrix = np.zeros(num_states, num_features)
+        # for state, state_index in states.items():
+        #     for feat, feat_val in state_featurizer(state).items():
+        #         assert isinstance(
+        #             feat_val, (int, float)), "value associated with feature needs to be a number"
+        #         feat_index = features.index(feat)
+        #         state_feature_matrix[state_index, feat_index] = feat_val
+
+        # self.state_feature_matrix = state_feature_matrix
+        # self.action_featurizer = action_featurizer
+
+    def learn(self, trajectories: FeaturesDataset):
+        '''
+        Parameters:
+          trajectories: array of trajectories which are dictionaries containing the state and action trajectory
         Output:
           model: trained PyTorch model
         '''
 
-        features_dataset = FeaturesDataset(
-            mdp, trajectories, transform=self.__transform__)
-        features_dataloader = DataLoader(
-            features_dataset, batch_size=batch_size, shuffle=True)
+        trajs_dataloader = DataLoader(
+            trajectories, batch_size=self.batch_size, shuffle=True)
 
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        model = model.to(device)
+        print(f"Using {device}\n")
 
-        for epoch in range(epochs):
+        classifier = self.model.to(device)
+
+        for epoch in range(self.epochs):
             print(f"Epoch {epoch+1}\n-------------------------------")
-            self.__train__(features_dataloader, model,
-                           loss_fn, optimizer, device)
+            self.__train(trajs_dataloader, classifier,
+                         self.loss_fn, self.optimizer, device)
             print()
-            if scheduler is not None:
-                scheduler.step()
+            if self.scheduler is not None:
+                self.scheduler.step()
         print("Done!")
 
-        return model
+        self.model = classifier
 
-    def __transform__(self, sample):
-        return torch.from_numpy(sample)
+        return classifier
 
-    def __train__(self, dataloader, model, loss_fn, optimizer, device):
+    def __train(self, dataloader, model, loss_fn, optimizer, device):
         size = len(dataloader.dataset)
         model.train()
         for batch, (X, y) in enumerate(dataloader):
--- dataset.py
+++ dataset.py
@@ -1,33 +1,34 @@
 from torch.utils.data import Dataset
 import numpy as np
-import torch
 
 
+# use transforms for transforming data for supervised learning
 class TrajectoryDataset(Dataset):
-    # trajectories format =  [{'state_traj': (1, 2, 3, 4, 5), 'action_traj': (2, 7, 9, 5, 1)}]
     def __init__(self, trajectories, transform=None, target_transform=None):
+        '''
+        Parameters:
+          trajectories: array of trajectories which are dictionaries containing 'state_traj' and 'action_traj' keys with tuple values
+          trajectories format example = [{'state_traj': (1, 2, 3, 4, 5), 'action_traj': (2, 7, 9, 5, 1)}]
+        '''
+        assert self.__check(
+            trajectories), "trajectories elements must conform to indicated structure"
+
         trajectories = np.array(trajectories)
 
-        self.num_trajs = len(trajectories)
-        self.trajectory_weights = torch.zeros(
-            self.num_trajs, dtype=torch.float64)
         self.state_trajectories = []
         self.action_trajectories = []
 
-        for traj_index, trajectory in enumerate(trajectories):
+        for trajectory in trajectories:
             self.state_trajectories.append(trajectory.get('state_traj'))
             self.action_trajectories.append(trajectory.get('action_traj'))
-            # need the normalization factor or else the gradients are too large
-            self.trajectory_weights[traj_index] = np.count_nonzero(
-                trajectories == trajectory) / self.num_trajs
 
         self.transform = transform
         self.target_transform = target_transform
 
     def __len__(self):
-        return self.num_trajs
+        return len(self.state_trajectories)
 
-    # an item is 1 trajectory (state_traj and corresponding action_traj) and also return the associated trajectory weight
+    # an item is 1 trajectory (state_traj and corresponding action_traj)
     def __getitem__(self, idx):
         state_traj = self.state_trajectories[idx]
         action_traj = self.action_trajectories[idx]
@@ -35,7 +36,21 @@ class TrajectoryDataset(Dataset):
             state_traj = self.transform(state_traj)
         if self.target_transform:
             action_traj = self.target_transform(action_traj)
-        return state_traj, action_traj, self.trajectory_weights[idx]
+        return state_traj, action_traj
+
+    def __check(self, trajectories):
+        str_arr = ['state_traj', 'action_traj']
+        for trajectory in trajectories:
+            if not isinstance(trajectory, dict):
+                return False
+            if len(trajectory.keys()) != 2:
+                return False
+            for traj_name, traj_sequence in trajectory.items():
+                if traj_name not in str_arr:
+                    return False
+                if not isinstance(traj_sequence, tuple):
+                    return False
+        return True
 
 
 class FeaturesDataset(Dataset):
@@ -44,7 +59,7 @@ class FeaturesDataset(Dataset):
         self.mdp = mdp
         self.color_features = sorted(mdp.feature_locations.keys())
         self.color_features = [f for f in self.color_features if f not in 'gs']
-        self.state_features, self.actions = self.__getFeaturesActions__(
+        self.state_features, self.actions = self.__getFeaturesActions(
             trajectories=trajectories)
         self.transform = transform
         self.target_transform = target_transform
@@ -62,7 +77,7 @@ class FeaturesDataset(Dataset):
         return state_feature, action
 
     # returns a label 0, 1, 2, 3, or 4
-    def __convertAction__(self, action):
+    def __convertAction(self, action):
         dy = action.get('dy')
         dx = action.get('dx')
         if dy == 1:
@@ -79,7 +94,7 @@ class FeaturesDataset(Dataset):
     # state feature vector: gets the features of a state which are just (x, y) coordinates
     # parameter state in form (frozendict({'x': 9, 'y': 0})
     # returns a 2D array [[x, y, state_identifier]]
-    def __getStateFeature__(self, state):
+    def __getStateFeature(self, state):
         f = self.mdp.location_features.get(state, '.')
         if f in self.color_features:
             color_index = self.color_features.index(f)
@@ -92,7 +107,7 @@ class FeaturesDataset(Dataset):
     # all_features: a 2D array where each row is a state (a sample) and column 0 is x-coordinate, column 1 is y-coordinate, column
     # 2 is state identifier
     # all_actions: a 1D array where each row corresponds to the action to take in the same row of all_features
-    def __getFeaturesActions__(self, trajectories):
+    def __getFeaturesActions(self, trajectories):
         all_features = []  # converting all states to features for training data
         all_actions = []  # storing the actions taken in each state in the same order
         for trajectory in trajectories:
@@ -103,12 +118,12 @@ class FeaturesDataset(Dataset):
             for state in state_traj:
                 # states are format (frozendict({'x': 9, 'y': 0})
                 # convert states to (x, y, state_identifier)
-                state_feature = self.__getStateFeature__(state)
+                state_feature = self.__getStateFeature(state)
                 all_features.append(state_feature)
             for action in action_traj:
                 # actions are format frozendict({'dy': 1, 'dx': 0})
                 # convert action to single dimension (an integer)
-                conv_action = self.__convertAction__(action)
+                conv_action = self.__convertAction(action)
                 all_actions.append(conv_action)
 
         # all_features must be a 2d array of floats and all_actions must be an array of integers
